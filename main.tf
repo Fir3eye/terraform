9a08339a32fdc353c6a345ffc64f39ea86112bf4
pallesanathkumar-design <pallesanathkumar@gmail.com>
2026-06-13 17:49:32 +0530

    first commit1

--- main.tf
+++ main.tf
@@ -13,6 +13,26 @@ resource "aws_instance" "test" {
 
 
 resource "aws_s3_bucket" "demo-bucket" {
-    bucket = "bucket-e334533453sdf"
-  
+  bucket = "bucket-e334533453sdf"
+
+}
+
+
+terraform {
+  required_version = ">= 1.0.0"
+
+  backend "s3" {
+    bucket         = "my-company-terraform-state-bucket-sai"
+    key            = "jenkins-pipeline/terraform.tfstate" # The path inside the bucket
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-locks" # Tells Terraform to lock using this table
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
